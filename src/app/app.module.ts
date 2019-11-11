89485cc298c5d869231b1e216f7ccbea4128ee4e
Sebastian <sebastian.valencian@gmail.com>
2019-11-10 22:01:30 -0500

    [Feat]Organizar las rutas de la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Modulos personalizados
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
